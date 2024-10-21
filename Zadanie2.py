39dd5cde5b8bc88899ae6677e4e5d03bcb0f3516
Imie <karolina.baumert@edu.uekat.pl>
2024-10-21 14:28:58 +0200

    zadanie1

---
+++ Zadanie2.py
@@ -0,0 +1,16 @@
+def multiply_by_two_for(numbers):
+    result = []
+    for number in numbers:
+        result.append(number * 2)
+    return result
+
+def multiply_by_two_comprehension(numbers):
+    return [number * 2 for number in numbers]
+
+numbers = [1, 2, 3, 4, 5]
+
+print("Wersja z użyciem pętli for:")
+print(multiply_by_two_for(numbers))
+
+print("Wersja z użyciem listy składającej:")
+print(multiply_by_two_comprehension(numbers))
